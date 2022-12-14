3a8be632e1340ad7345a921279d415bfdcd6ac35
kiborg7075@gmail.com <kiborg7075@gmail.com>
2022-12-14 12:12:33 +0300

    feat :edit HTML and CSS

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 $( document ).ready(function(){
-    $(".mobile_on").click(function(){ 
-     $(".nav_bar").toggleClass('mobile_bg'); 
-    });
+  function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".nav_bar");
+    if(windowWidth < 480){
+      elem.addClass('mobile_bg');
+    }
+    else{
+      elem.removeClass('mobile_bg');
+    }
+  }
+  checkWidth();
+  $(window).resize(function(){
+    checkWidth();
   });
+  $(".mobile_on").click(function(){ 
+  $(".nav_bar").toggleClass('mobile_bg'); 
+    });
+//  $('.en').addClass('dis');
+//  $( ".first" ).hover(function(){
+//	  $( this ).toggleClass( "under" );
+//  $( ".en" ).toggleClass( "dis" ) ;
+//	 });
+	});
